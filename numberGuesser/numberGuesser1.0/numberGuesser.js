b7768e22308181e20fba256dae6c9b4c31a06111
Nick Danvers <ndanvers222@gmail.com>
2020-01-04 14:02:28 -0800

    Update numberGuesser.js

--- numberGuesser/numberGuesser1.0/numberGuesser.js
+++ numberGuesser/numberGuesser1.0/numberGuesser.js
@@ -1,52 +1,51 @@
-  const numberGuesser = function(){
-    let answerNumber = Math.ceil(Math.random()*100);
+ const numberGuesser = function(){
+  let answerNumber = Math.ceil(Math.random()*100);
 
-    // the prompt & variables
-    let prompt = require("prompt-sync")();
-    console.log("Guess a number: ")
-    let number = prompt("> "  );
-    let numberOfAttempts = 0;
-    let previousGuess;
+  // the prompt & variables
+  let prompt = require("prompt-sync")();
+  console.log("Guess a number: ")
+  let number = prompt("> "  );
+  let numberOfAttempts = 0;
+  let previousGuess;
 
-    //looping while loop for the prompt
-  while (number !== answerNumber){
+  //looping while loop for the prompt
+while (number !== answerNumber){
 
-    //If its not a number
-      if(isNaN(number)) {
-        console.log("Thats not a number");
-        console.log("Guess a number: ")
-        number = prompt("> ")
-      } 
+  //If its not a number
+    if(isNaN(number)) {
+      console.log("Thats not a number");
+      console.log("Guess a number: ")
+      number = prompt("> ")
+    } 
 
-    // If its the same as the previous guess
-      else if (number == previousGuess) {
-        console.log("You've already said that!")
-        console.log("Guess a number: ");
-        number = prompt("> ")
-      } 
-    // too low
-      else if(number < answerNumber) {
-        console.log("Too Low!")
-        previousGuess = number;
-        console.log("Guess a number: ")
-        number = prompt("> ");
-        numberOfAttempts++;
-    }
-    // too high
-     else if (number > answerNumber) {
-        console.log("Too High!")
-        previousGuess = number;
-        console.log("Guess a number: ")
-        number = prompt(">  ")
-        numberOfAttempts++;
-    // success
-    } else if (number == answerNumber){
-      break;     
-    
+  // If its the same as the previous guess
+    else if (number == previousGuess) {
+      console.log("You've already said that!")
+      console.log("Guess a number: ");
+      number = prompt("> ")
+    } 
+  // too low
+    else if(number < answerNumber) {
+      console.log("Too Low!")
+      previousGuess = number;
+      console.log("Guess a number: ")
+      number = prompt("> ");
+      numberOfAttempts++;
   }
-  }
-  return "You got it! You took " + numberOfAttempts + " attempts!";
-  }
+  // too high
+   else if (number > answerNumber) {
+      console.log("Too High!")
+      previousGuess = number;
+      console.log("Guess a number: ")
+      number = prompt(">  ")
+      numberOfAttempts++;
+  // success
+  } else if (number == answerNumber){
+    break;     
   
-  console.log(numberGuesser());
-  
+}
+}
+return "You got it! You took " + numberOfAttempts + " attempts!";
+}
+
+console.log(numberGuesser());
